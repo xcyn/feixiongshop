d816a9b8b4a0f7d7227165d6365e9898f4a79913
xiongchuanyu <18811742305@163.com>
2021-11-01 18:08:31 +0800

    feat: 更新默认地址接口

--- servece/controller/address.js
+++ servece/controller/address.js
@@ -80,6 +80,51 @@ appRouter.post('/update-address', async(ctx, next) => {
   })
 })
 
+// 编辑默认地址
+appRouter.post('/update-default-address', async(ctx, next) => {
+  let { 
+    userId,
+    telNumber,
+    id
+  } = ctx.request.body
+  const sequelize = await ctx.state.orm.db(database).sequelize()
+  // 开启事务
+  let t = await sequelize.transaction()
+  try {
+    const addressTable = await ctx.state.orm.db(database).table('address').getTable('address', sequelize)
+    await addressTable.sync({ force: false }); //创建表
+    let curAddress = await addressTable.findAll({
+      where: {
+        userId,
+        telNumber,
+      }
+    }, {transaction: t})
+    for(let i = 0; i < curAddress.length; i++) {
+      let idIns = curAddress[i].id
+      const isDefault = id === idIns
+      await addressTable.update({
+        isDefault: isDefault
+      }, {
+        where: {
+          id: idIns
+        }
+      });
+    }
+    t.commit();
+    ctx.state.res({
+      data: {
+        curAddress
+      }
+    })
+  } catch (error) {
+    t.rollback();
+    ctx.state.res({
+      errno: 1000,
+      errmsg: `更新默认地址:${error}`,
+    })
+  }
+})
+
 // 查询地址
 appRouter.get('/select-address', async(ctx, next) => {
   let { 
