2c3a6b402504108b8d63b022610ffe72fa446515
xiongchuanyu <18811742305@163.com>
2021-12-21 12:25:40 +0800

    feat: add log

--- servece/config/index.js
+++ servece/config/index.js
@@ -4,6 +4,7 @@ if(!process.env.ENV) {
   throw new Error(`环境变量设置有误!`)
 }
 const dbConfig = config[process.env.ENV];
+console.log('数据库配置为:',JSON.stringify(dbConfig))
 module.exports = {
   dbConfig
 }
