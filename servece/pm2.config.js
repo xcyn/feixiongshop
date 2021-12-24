be648d49ed320e8b05dc9ef3c448e48b82cfeea3
xiongchuanyu <18811742305@163.com>
2021-12-24 12:13:30 +0800

    feat: test

--- servece/pm2.config.js
+++ servece/pm2.config.js
@@ -5,15 +5,15 @@ module.exports = {
           name: "feixiongshop-serve",
           script: "index.js",
           // watch: true,
-          ignore_watch: [
-              // 从监控目录中排除
-              "node_modules",
-              "static",
-              "logs",
-              "log-dev",
-              "log-pro",
-              "crash"
-          ],
+          // ignore_watch: [
+          //     // 从监控目录中排除
+          //     "node_modules",
+          //     "static",
+          //     "logs",
+          //     "log-dev",
+          //     "log-pro",
+          //     "crash"
+          // ],
         // "log_date_format": "YYYY-MM-DD HH:mm",
         // "out_file": isDev ? "log-dev/out_file.log" : "log-pro/out_file.log",
         // "error_file": isDev ? "log-dev/error_file.log" : "log-pro/error_file.log",
