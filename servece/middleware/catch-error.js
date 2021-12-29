09bc1ff9837d50ed3c896776920d81e0078e62c8
xiongchuanyu <18811742305@163.com>
2021-12-29 14:52:27 +0800

    feat: fix bug

--- servece/middleware/catch-error.js
+++ servece/middleware/catch-error.js
@@ -1,6 +1,8 @@
 module.exports = (ctx, next) => {
   return next().catch(err => {
-    console.log('err', err)
+    console.log(`serve-log:全局catch错误:
+      err: ${err}
+    `)
     // 处理鉴权
     if(err && err.status === 401) {
       ctx.state.res({
@@ -8,7 +10,7 @@ module.exports = (ctx, next) => {
         errmsg: '登录失效，请重新授权登录',
         data: ''
       })
-    } else if(err === 'jwt expired') {
+    } else if(err && err.name === 'TokenExpiredError') {
       ctx.state.res({
         errno: 401,
         errmsg: '登录失效，请重新授权登录',
