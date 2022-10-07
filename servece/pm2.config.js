65d2659c8911a279d5ee67e1c4fdd4975a12713a
apple <xiongchuanyu@qiyi.com>
2022-10-07 18:09:08 +0800

    feat: fix

--- servece/pm2.config.js
+++ servece/pm2.config.js
@@ -2,9 +2,22 @@ const isDev = process.env.NODE_ENV === "dev";
 module.exports = {
   apps: [
       {
-          name: "feixiongshop-serve",
-          script: "index.js",
+        name: "feixiongshop-serve",
+        script: "index.js",
         instances: 'max',
+        watch: false,
+        ignore_watch: [
+            // 从监控目录中排除
+            "node_modules",
+            "public",
+            "static",
+            "logs",
+            "log-dev",
+            "log-pro",
+            "mock-data",
+            "log",
+            "crash"
+        ],
         max_memory_restart: '150M'
       }
   ]
